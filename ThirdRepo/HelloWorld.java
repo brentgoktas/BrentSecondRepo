5b78d7d32256a81fe8fa0038fa38cebe4d53f4b5
brentgoktas <brentgoktas@gmail.com>
2022-07-05 20:20:30 -0400

    my Messages

--- ThirdRepo/HelloWorld.java
+++ ThirdRepo/HelloWorld.java
@@ -1,2 +1,16 @@
-package ThirdRepo;public class HelloWorld {
+package ThirdRepo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
+//    git status
+//    git add *
+//        git status (if its not recognized)
+//        git commit -m"Your message"
+//        git push
+//        (if you use first time add this also)
+//        git branch -M main
+//        git remote add origin https........address
+//        git push -u origin main
